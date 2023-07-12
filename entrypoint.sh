1865e2dc50718f3b2c9066325fa1daefd2c37a5e
Mathew <mat999@gmail.com>
2023-07-12 19:53:20 +1000

    use our docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,6 @@ SLAVE2_PORT=$5
 SLAVE3_PORT=$6
 
 
-docker_run="docker run -d -p ${MASTER1_PORT}:6379 -p ${MASTER2_PORT}:6380 -p ${MASTER3_PORT}:6381 -p ${SLAVE1_PORT}:6382 -p ${SLAVE2_PORT}:6383 -p ${SLAVE3_PORT}:6384 --name redis-cluster vishnunair/docker-redis-cluster:latest"
+docker_run="docker run -d -p ${MASTER1_PORT}:6379 -p ${MASTER2_PORT}:6380 -p ${MASTER3_PORT}:6381 -p ${SLAVE1_PORT}:6382 -p ${SLAVE2_PORT}:6383 -p ${SLAVE3_PORT}:6384 --name redis-cluster x4bnet/redis-cluster:latest"
 
 sh -c "$docker_run"
